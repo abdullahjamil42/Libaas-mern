57d273c8cd343b17f661c4167deed9c03ee1b279
Abdullah Jamil <129326650+abdullahjamil42@users.noreply.github.com>
2025-07-06 18:17:55 +0500

    disabled mailgun.js

--- estore/backend/config/mailgun.js
+++ estore/backend/config/mailgun.js
@@ -1,29 +1,29 @@
-const mailgun = require('mailgun-js');
-const DOMAIN = process.env.MAILGUN_DOMAIN;
-const mg = mailgun({ apiKey: process.env.MAILGUN_API_KEY, domain: DOMAIN });
+// const mailgun = require('mailgun-js');
+// const DOMAIN = process.env.MAILGUN_DOMAIN;
+// const mg = mailgun({ apiKey: process.env.MAILGUN_API_KEY, domain: DOMAIN });
 
-// Send email function
-const sendEmail = (recipientEmail, subject, body) => {
-  const data = {
-    from: process.env.FROM_EMAIL,
-    to: recipientEmail,
-    subject: subject,
-    text: body,
-    html: `<html><body><h1>${body}</h1></body></html>`,
-  };
+// // Send email function
+// const sendEmail = (recipientEmail, subject, body) => {
+//   const data = {
+//     from: process.env.FROM_EMAIL,
+//     to: recipientEmail,
+//     subject: subject,
+//     text: body,
+//     html: `<html><body><h1>${body}</h1></body></html>`,
+//   };
 
-  // Debug log to check if domain and mg are correct
-  console.log("Mailgun API Key:", process.env.MAILGUN_API_KEY);
-  console.log("Mailgun Domain:", DOMAIN);
-  console.log("Mailgun Object:", mg);
+//   // Debug log to check if domain and mg are correct
+//   console.log("Mailgun API Key:", process.env.MAILGUN_API_KEY);
+//   console.log("Mailgun Domain:", DOMAIN);
+//   console.log("Mailgun Object:", mg);
 
-  mg.messages().send(data, (error, body) => {
-    if (error) {
-      console.error('Error sending email:', error);
-    } else {
-      console.log('Email sent successfully:', body);
-    }
-  });
-};
+//   mg.messages().send(data, (error, body) => {
+//     if (error) {
+//       console.error('Error sending email:', error);
+//     } else {
+//       console.log('Email sent successfully:', body);
+//     }
+//   });
+// };
 
-module.exports = sendEmail;
+// module.exports = sendEmail;
